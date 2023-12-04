7c8f52955c911ec371ff88318aab1fdeb53eba96
Cristian Cajiao <cristiancajiaos@outlook.com>
2023-12-04 11:51:51 -0300

    Agregados pasos de Joyride al módulo Home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { JoyrideService } from 'ngx-joyride';
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private readonly joyrideService: JoyrideService
+  ) { }
 
   ngOnInit() {
+    setTimeout(() => {
+      this.joyrideService.startTour({
+        steps: ['pasoUno', 'pasoDos', 'pasoTres']
+      });
+    }, 1000);
   }
 
 }
